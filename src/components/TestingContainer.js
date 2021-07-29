c4bbe259579e803ac34f754379d521cd1580720e
MichaelAbe <michael.k.abe@gmail.com>
2021-07-29 15:59:04 -0400

    setup testing container

---
+++ src/components/TestingContainer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Testing from './Testing'
+
+export default function testingContainer() {
+    return (
+        <div>
+            <h1>This is the testing container </h1>     
+         </div>
+    )
+}
